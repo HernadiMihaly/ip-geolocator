d424b2ee8e29fbe29009ddf12114389df0853ba4
Hernádi Mihály <hernadi.mihaly99@gmail.com>
2022-03-10 13:54:02 +0100

    Plugins added, shrek site deployed

--- src/main/java/geolocator/Main.java
+++ src/main/java/geolocator/Main.java
@@ -1,7 +1,13 @@
 package geolocator;
 
+/**
+ * A Main osztály
+ */
 public class Main {
-
+    /**
+     * A main metódus
+     * @param args A parancssori argumentumokat tartalmazó tömb
+     */
     public static void main(String[] args) {
         if (args.length > 1) {
             System.err.printf("Usage: java %s [ipOrHostName]%n", Main.class.getName());
